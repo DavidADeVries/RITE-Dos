a28f22262db9fc2bebb0819b48829d8f159f90b1
David DeVries <DavidADeVries@users.noreply.github.com>
2017-11-10 17:30:04 -0500

    Started Beam Edit GUI dev

--- Classes/CommissionedEnergy.m
+++ Classes/CommissionedEnergy.m
@@ -5,7 +5,8 @@ classdef CommissionedEnergy
     
     properties
         energyInMeV = 6 % MeV
-        doseRateInMUsPerSecond = 400 %MU/s
+        deliveredMU = 100 % MUs
+        doseRateInMUsPerMinute = 400 %MU/min
         
         notes = ''
         
@@ -13,8 +14,10 @@ classdef CommissionedEnergy
         phantomWidthsInCm = 5:5:40 % cm
         phantomShiftsInCm = -10:5:10 % cm
         
-        tissueMaximumRatios = {} % not from RITE-Dos!        
-        commissioningEpidResults = {} % cell array of CommissioningEpidResult
+        commissioningEpidDataPath = ''
+        commissioningEpidDataDirectoriesCreated = false %T/F, set to T after directories made
+        
+        tissuePhantomRatioPath = ''
     end
     
     methods
@@ -22,8 +25,50 @@ classdef CommissionedEnergy
             name = [...
                 num2str(this.energyInMeV),...
                 'MeV @ ',...
-                num2str(this.doseRateInMUsPerSecond),...
-                'MU/s'];
+                num2str(this.doseRateInMUsPerMinute),...
+                'MU/min'];
+        end
+        
+        function [] = setGUI(this, app)
+            app.Beam_Edit_BeamEnergyEditField.Value = this.energyInMeV;
+            app.Beam_Edit_DeliveredMUsEditField.Value = this.deliveredMU;
+            app.Beam_Edit_DoseRateEditField.Value = this.doseRateInMUsPerMinute;
+            app.Beam_Edit_NotesTextArea.Value = this.notes;
+            
+            app.Beam_Edit_FieldSizesEditField.Value = getIntervalStringFromValues(this.fieldSizesInCm);
+            app.Beam_Edit_PhantomWidthsEditField.Value = getIntervalStringFromValues(this.phantomWidthsInCm);
+            app.Beam_Edit_PhantomShiftsEditField.Value = getIntervalStringFromValues(this.phantomShiftsInCm);
+            
+            % lock down fields if directories already created
+            if this.commissioningEpidDataDirectoriesCreated
+                app.Beam_Edit_FieldSizesEditField.Editable = 'off';
+                app.Beam_Edit_PhantomWidthsEditField.Editable = 'off';
+                app.Beam_Edit_PhantomShiftsEditField.Editable = 'off';
+                
+                app.Beam_Edit_CommissioningDataPathSelectButton.Enable = 'off';
+                app.Beam_Edit_CommissioningDataDirectoriesCreateButton.Enable = 'off';
+            else
+                app.Beam_Edit_FieldSizesEditField.Editable = 'on';
+                app.Beam_Edit_PhantomWidthsEditField.Editable = 'on';
+                app.Beam_Edit_PhantomShiftsEditField.Editable = 'on';
+                
+                app.Beam_Edit_CommissioningDataPathSelectButton.Enable = 'on';
+                app.Beam_Edit_CommissioningDataDirectoriesCreateButton.Enable = 'on';
+            end
+            
+            % only allow Create Directories if path selected
+            if isempty(this.commissioningEpidDataPath)
+                app.Beam_Edit_CommissioningDataPathEditField.Value = 'No path selected';
+                app.Beam_Edit_CommissioningDataDirectoriesCreateButton.Enable = 'off';
+            else                
+                app.Beam_Edit_CommissioningDataPathEditField.Value = this.commissioningEpidDataPath;
+                
+                if ~this.commissioningEpidDataDirectoriesCreated
+                    app.Beam_Edit_CommissioningDataDirectoriesCreateButton.Enable = 'on';                
+                end
+            end
+            
+            
         end
     end
     
