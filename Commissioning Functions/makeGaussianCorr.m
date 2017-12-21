98673e620a3095a21be5532ba9cd40fb144c38fd
David DeVries <DavidADeVries@users.noreply.github.com>
2017-12-21 14:58:06 -0500

    Refactoring

--- Commissioning Functions/makeGaussianCorr.m
+++ Commissioning Functions/makeGaussianCorr.m
@@ -2,17 +2,15 @@ function [ws_in, ws_cr, DoseConvs, mat_HornCorr] = makeGaussianCorr(tpsValues, e
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+startingStDev = 1.7;
 
+stDevMultipliers = [1, 2, 10, 30];
+numGaussians = length(stDevMultipliers);
 
-SD1 = 1.7;
-SD2 = SD1*2;
-SD3 = SD1*10;
-SD4 = SD1*30;
+stDevs = startingStDev .* stDevMultipliers;
 
-stDevs = [SD1, SD1, SD3, SD4];
-
-x=1:1000;
-m=(max(x)-min(x))/2+0.5;
+x = 1:1000;
+m = (max(x)-min(x))/2+0.5;
 
 gaussians = gaussDistribution(x, m, stDevs/0.523);
 
@@ -21,10 +19,10 @@ gaussians = gaussDistribution(x, m, stDevs/0.523);
 mat_HornCorr = zeros(size(epidData_F));
 DoseConvs = zeros(size(epidData_F));
 
-ws_cr = zeros(4,size(epidData_F,3));
-ws_in = zeros(4,size(epidData_F,3));
+ws_cr = zeros(numGaussians, size(epidData_F,3));
+ws_in = zeros(numGaussians ,size(epidData_F,3));
 
-[crossPlaneWindow, inPlaneWindow] = getCentralAveragingWindow(epidDims, settings.centralAveragingWindowSideLength);
+% [crossPlaneWindow, inPlaneWindow] = getCentralAveragingWindow(epidDims, settings.centralAveragingWindowSideLength);
 
 for i=1:size(epidData_F,3)
     % What I'll do is use the w_s and l_s to index properly rather than
@@ -44,25 +42,23 @@ for i=1:size(epidData_F,3)
     fmap = ones(epidDims(2), epidDims(1));
     TMRratio = ones(epidDims(2), epidDims(1));
     
-    % Adjusts the EPIDs for left-right and superior-inferior displacement.
-    epid_max = mean2(epidData_F(inPlaneWindow, crossPlaneWindow ,i));
-    epid_min = mean2(epidData_F(1:8,1:8,i));
-    mask = (epidData_F(:,:,i) > abs(epid_max+epid_min)/2);
-    
-    tps_max = mean2(tpsValues(inPlaneWindow, crossPlaneWindow, i));
-    tps_mask = (tpsValues(:,:,i)>abs(tps_max)/2);
-    
-    mask_diff = mask-tps_mask;
-        
+%     % Adjusts the EPIDs for left-right and superior-inferior displacement.
+%     epid_max = mean2(epidData_F(inPlaneWindow, crossPlaneWindow ,i));
+%     epid_min = mean2(epidData_F(1:8,1:8,i));
+%     mask = (epidData_F(:,:,i) > abs(epid_max+epid_min)/2);
+%     
+%     tps_max = mean2(tpsValues(inPlaneWindow, crossPlaneWindow, i));
+%     tps_mask = (tpsValues(:,:,i)>abs(tps_max)/2);
+       
 
     shiftEPIDsF = epidData_F(:,:,i);
-    epid_elements=sort(shiftEPIDsF(:),'descend');
-    epid_64_max=mean(epid_elements(101:151));
-    epid_64_min=mean(epid_elements(end-150:end-100));
-    mask_epid=+(shiftEPIDsF>abs(epid_64_max+epid_64_min)/2);
+    epid_elements = sort(shiftEPIDsF(:),'descend');
+    epid_64_max = mean(epid_elements(101:151));
+    epid_64_min = mean(epid_elements(end-150:end-100));
+    mask_epid = +(shiftEPIDsF>abs(epid_64_max+epid_64_min)/2);
     
     % In-plane gaussian
-    [weightsInPlane, ConvRescaleFactor_in]=Fit2GaussConv(shiftEPIDsF(:,256)',tpsValues(:,256,i)',SD1,SD2,SD3,SD4);
+    weightsInPlane = Fit2GaussConv(shiftEPIDsF(:,256)',tpsValues(:,256,i)', stDevs);
 
     ws_in(:,i) = weightsInPlane;
     
@@ -70,11 +66,11 @@ for i=1:size(epidData_F,3)
         
     % Cross-plane gaussian
     
-    [weightsCrossPlane, ConvRescaleFactor_cross]=Fit2GaussConv(shiftEPIDsF(192,:),tpsValues(192,:,i),SD1,SD2,SD3,SD4);
+    weightsCrossPlane = Fit2GaussConv(shiftEPIDsF(192,:),tpsValues(192,:,i), stDevs);
 
     ws_cr(:,i) = weightsCrossPlane;
     
-    gsumcr = (sum(weightsCrossPlane.*gaussians)/trapz(sum(weightsCrossPlane.*gaussians))):
+    gsumcr = (sum(weightsCrossPlane.*gaussians)/trapz(sum(weightsCrossPlane.*gaussians)));
 
     
     
